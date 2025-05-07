cf057e2ff6ebba095ee55e110341f49abbbddbfd
Yogesh Mali <apple@Yogeshs-MacBook-Pro.local>
2025-05-07 15:58:06 +0530

    Setup Auth Layouts, Social Auth Forms, Next Auth, OAuth with Google and Github Providers

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/route";
 
 const Home = async () => {
   const session = await auth()
@@ -7,6 +9,14 @@ const Home = async () => {
   return (
     <>
       <h1 className="h1-bold">Hello world!</h1>
+
+      <form className="px-10 mt-[100px]" action={async () => {
+        "use server";
+
+        await signOut({redirectTo: ROUTES.SIGN_IN})
+      }}>
+        <Button className="cursor-pointer" type="submit" >Log out</Button>
+      </form>
     </>
   );
 }
